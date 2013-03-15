b11630a1bfa8dd353f65383ef419eb93f4008033
Kendra Yee <cunningham.kendra@gmail.com>
2013-03-15 12:04:35 -0400

    update about and home
    :wq

--- Home.php
+++ Home.php
@@ -1,1 +1,11 @@
-<h1> Calculate My Closet </h1>
+<head>
+    <title>Calculate My Closet</title>
+<head>
+<body>
+    <h1 style="font-family:Futura">Calculate My Closet</h1>
+    <h3 style="font-family:Futura">By Kendra Cunningham, Stacey Lavender, and Jeff Warner</h3>
+    <a href="createaccount.php" style="font-family:Futura">Lets Get Started!</a>
+        <p></p>
+    <a href="about.php" style="font-family:Futura">What is this?</a>
+        <p></p>
+</body>
--- about.php
+++ about.php
@@ -3,5 +3,5 @@
 <head>
 <body>
     <h1 style="font-family:Futura">About Calculate My Closet</h1>
-        <p style="font-family:Futura; font-size:12px"></p>
+        <p style="font-family:Futura; font-size:12px">Calculate My Closet is a ...</p>
 </body>
