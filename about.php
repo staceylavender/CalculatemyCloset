34a1c1951463cf5fad3812c10732916f5ceaa821
Kendra Yee <cunningham.kendra@gmail.com>
2013-03-15 12:02:58 -0400

    Added about page

---
+++ about.php
@@ -0,0 +1,7 @@
+<head>
+    <title>Calculate My Closet</title>
+<head>
+<body>
+    <h1 style="font-family:Futura">About Calculate My Closet</h1>
+        <p style="font-family:Futura; font-size:12px"></p>
+</body>
