91eb52e45cb36a63bf845c8f4798c538b395645f
Kendra Yee <cunningham.kendra@gmail.com>
2013-03-18 17:01:55 -0400

    Added functionality

--- organize2.php
+++ organize2.php
@@ -1,47 +1,174 @@
 <!--This is the second page for users to design their database-->
 <!--Here users will select the values for their database-->
 <!--Values selected here will appear in drop down select lists on organize3 page.--> 
+<head>
+    <title>Calculate My Closet</title>
+<head>
+<body>
+    <h1 style="font-family:Futura; color:coral">Calculate My Closet</h1>
+    <h2 style="font-family:Futura">Organize Your Closet</h2>
+    
+    <style type="text/css">
+    #wrap {
+        width:800px;
+        margin:0 auto; 
+        }
+        
+    #left_col {
+        float:left;
+        width:400px; 
+        height: 160px;
+        }
+        
+    #right_col {
+        float:right;
+        width:400px; 
+        height: 160px;
+        }
+         
+    </style>
 
-<h1 style="font-family:Futura; color:coral">Design Your Database</h1>
-<p style="font-family:Futura">Below select all fields you would like to use in your Database</p>
+    <div id="wrap">
+        <div id="left_col">
+            <p style="font-family:Futura">Types of Clothing</p>
+            <p>Select the Types of clothes in your closet</p>
+            <input type="button" name="type" value="Jeans" />
+            <input type="button" name="type" value="Pants" />
+            <input type="button" name="type" value="Shorts" />
+            <input type="button" name="type" value="Skirt" />
+            <input type="button" name="type" value="Dress" />
+            <input type="button" name="type" value="Tshirt" />
+            <input type="button" name="type" value="Shirt" />
+            <input type="button" name="type" value="Blouse" />
+            <input type="button" name="type" value="Coat" />
+            <input type="button" name="type" value="Jacket" />
+            <input type="button" name="type" value="Athletic Top" />
+            <input type="button" name="type" value="Athletic Bottom" />
+        </div>
+    </div>
 
-<p style="font-family:Futura">Choose a name for your Database:</p>
-<textarea name="name" cols="20" rows="1" wrap="off"> </textarea>
-<p style="font-family:Futura">Types of Clothing</p>
-Jeans <input type="checkbox" name="type" value="Jeans" />
-Pants <input type="checkbox" name="type" value="Pants" />
-Shorts <input type="checkbox" name="type" value="Shorts" />
-Skirts <input type="checkbox" name="type" value="Skirt" />
-Dresses <input type="checkbox" name="type" value="Dress" />
-T-Shirt <input type="checkbox" name="type" value="Tshirt" />
-Shirt <input type="checkbox" name="type" value="Shirt" />
-Blouse <input type="checkbox" name="type" value="Blouse" />
-Coat <input type="checkbox" name="type" value="Coat" />
-Jacket <input type="checkbox" name="type" value="Jacket" />
+    <div id="wrap">
+        <div id="right_col">
+            <p style="font-family:Futura">Price Range</p>
+            <p>Select Price Ranges</p>
+            <input type="button" name="price" value="$10 or less"/>
+            <input type="button" name="price" value="$11-$15"/>
+            <input type="button" name="price" value="$16-$20"/>
+            <input type="button" name="price" value="$21-$30"/>
+            <input type="button" name="price" value="$31-$40"/>
+            <input type="button" name="price" value="$41-$50"/>
+            <input type="button" name="price" value="$51-$60"/>
+            <input type="button" name="price" value="$61-$70"/>
+            <input type="button" name="price" value="$71-$80"/>
+            <input type="button" name="price" value="$81-$100"/>
+            <input type="button" name="price" value="$100-$150"/>
+            <input type="button" name="price" value="$150-$200"/>
+        </div
+    </div>
 
-<p style="font-family:Futura">Price Range</p>
-0-15 <input type="checkbox" name="price" value="Fifteen" />
-15-25 <input type="checkbox" name="price" value="TwentyFive" />
-25-50 <input type="checkbox" name="price" value="Fifty" />
-50-75 <input type="checkbox" name="price" value="SeventyFive" />
-75-100 <input type="checkbox" name="price" value="Hundred" />
-100-150 <input type="checkbox" name="price" value="HundredFifty" />
-150+ <input type="checkbox" name="price" value="Over" />
+    <div id="wrap">
+        <div id="left_col">
+            <p style="font-family:Futura">Color</p>
+            <p>Select the colors in your closet</p>
+            <input type="button" name="color" value="White" />
+            <input type="button" name="color" value="Black" />
+            <input type="button" name="color" value="Gray" />
+            <input type="button" name="color" value="Red" />
+            <input type="button" name="color" value="Orange" />
+            <input type="button" name="color" value="Yellow" />
+            <input type="button" name="color" value="Green" />
+            <input type="button" name="color" value="Blue" />
+            <input type="button" name="color" value="Purple" />
+            <input type="button" name="color" value="Brown" />
+            <input type="button" name="color" value="Peach" />
+            <input type="button" name="color" value="Mint" />
+            <input type="button" name="color" value="Magenta" />
+            <input type="button" name="color" value="Off-White" />
+        </div>
+    </div>
+    
+    <div id="wrap">
+        <div id="right_col">
+            <p style="font-family:Futura">Patterns</p>
+            <p>Select the patterns in your closet</p>
+            <input type="button" name="pattern" value="Striped" />
+            <input type="button" name="pattern" value="Multi" />
+            <input type="button" name="pattern" value="Dots" />
+            <input type="button" name="pattern" value="Paisley" />
+            <input type="button" name="pattern" value="Argyle" />
+            <input type="button" name="pattern" value="Checkered" />
+            <input type="button" name="pattern" value="Floral" />
+            <input type="button" name="pattern" value="Brocade" />
+            <input type="button" name="pattern" value="Houndstooth" />
+            <input type="button" name="pattern" value="Tartan" />
+            <input type="button" name="pattern" value="Chevron/ZigZag" />
+            <input type="button" name="pattern" value="Toile" />
+            <input type="button" name="pattern" value="Tie-Dye" />
+        </div>
+    </div>
+    
+    <div id="wrap">
+        <div id="left_col">
+            <p style="font-family:Futura">Clothes Size</p>
+            <p>Select the sizes in your closet</p>
+            <input type="button" name="pattern" value="XXS" />
+            <input type="button" name="pattern" value="XS" />
+            <input type="button" name="pattern" value="S" />
+            <input type="button" name="pattern" value="M" />
+            <input type="button" name="pattern" value="L" />
+            <input type="button" name="pattern" value="XL" />
+            <input type="button" name="pattern" value="XXL" />
+            <input type="button" name="pattern" value="00" />
+            <input type="button" name="pattern" value="0" />
+            <input type="button" name="pattern" value="2" />
+            <input type="button" name="pattern" value="4" />
+            <input type="button" name="pattern" value="6" />
+            <input type="button" name="pattern" value="8" />
+            <input type="button" name="pattern" value="10" />
+            <input type="button" name="pattern" value="12" />
+            <input type="button" name="pattern" value="14" />
+            <input type="button" name="pattern" value="16" />
+            <input type="button" name="pattern" value="18" />
+            <input type="button" name="pattern" value="20" />
+            <input type="button" name="pattern" value="22" />
+            <input type="button" name="pattern" value="24" />
+        </div>
+    </div>
+    
+    <div id="wrap">
+        <div id="right_col">
+            <p style="font-family:Futura">Pants Size</p>
+            <p>Select the sizes of your jeans and pants in your closet</p>
+            <input type="button" name="pattern" value="26" />
+            <input type="button" name="pattern" value="27" />
+            <input type="button" name="pattern" value="28" />
+            <input type="button" name="pattern" value="29" />
+            <input type="button" name="pattern" value="30" />
+            <input type="button" name="pattern" value="31" />
+            <input type="button" name="pattern" value="32" />
+            <input type="button" name="pattern" value="33" />
+        </div>
+    </div>
+    
+    <div id="wrap">
+        <div id="left_col">
+            <p style="font-family:Futura">Fit</p>
+            <p>Select Fit</p>
+            <input type="button" name="pattern" value="Regual" />
+            <input type="button" name="pattern" value="Bootcut" />
+            <input type="button" name="pattern" value="Skinny" />
+            <input type="button" name="pattern" value="Low-rise" />
+            <input type="button" name="pattern" value="Flare" />
+            <input type="button" name="pattern" value="Baggy" />
+            <input type="button" name="pattern" value="Fitted" />
+            <input type="button" name="pattern" value="Shift" />
+        </div>
+    </div>
+    
+    <div id="wrap">
+        <div id="right_col">
+            <input style="background-color:coral" type="button" value="Create Database!" onclick="location.href='organize3.php'; return false">
+        </div>
+    </div>
 
-<p style="font-family:Futura">Color and Patterns</p>
-White <input type="checkbox" name="color" value="White" />
-Black <input type="checkbox" name="color" value="Black" />
-Gray <input type="checkbox" name="color" value="Gray" />
-Red <input type="checkbox" name="color" value="Red" />
-Orange <input type="checkbox" name="color" value="Orange" />
-Yellow <input type="checkbox" name="color" value="Yellow" />
-Green <input type="checkbox" name="color" value="Green" />
-Blue <input type="checkbox" name="color" value="Blue" />
-Purple <input type="checkbox" name="color" value="Purple" />
-Brown <input type="checkbox" name="color" value="Brown" />
-Multi Polka Dots <input type="checkbox" name="color" value="Multi" />
-Polka Dots <input type="checkbox" name="color" value="Dots" />
-Paisley <input type="checkbox" name="color" value="Paisley" />
-
-<p><input type="button" value="Create Database"
-  onclick="location.href='database.php'; return false"></p>
+</body>
