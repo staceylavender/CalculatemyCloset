152f21405b0dc38df037c72ace0e497e729adc58
Kendra Yee <cunningham.kendra@gmail.com>
2013-03-18 16:55:41 -0400

    Update page

--- organize1.php
+++ organize1.php
@@ -1,2 +1,9 @@
 <!--First page for users to design their database-->
 <!--Here users will 'Turn On' their desired fields-->
+<head>
+    <title>Calculate My Closet</title>
+<head>
+<body>
+    <h1 style="font-family:Futura">Calculate My Closet</h1>
+    <h3 style="font-family:Futura">Create Your Closet</h3>
+    <p style="font-family:Futura; font-size:13px">Select the categories you want to use in your closet</p>
