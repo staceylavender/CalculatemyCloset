531a9c02b4b29bae5e50f674832359f9fdbb91de
Kendra Yee <cunningham.kendra@gmail.com>
2013-03-25 15:30:19 -0400

    Fixed toggle

--- organize2.php
+++ organize2.php
@@ -1,12 +1,23 @@
 <!--This is the second page for users to design their database-->
 <!--Here users will select the values for their database-->
 <!--Values selected here will appear in drop down select lists on organize3 page.--> 
+<?php session_start(); ?>
 <head>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js">
+    </script>
     <title>Calculate My Closet</title>
 <head>
 <body>
     <h1 style="font-family:Futura; color:coral">Calculate My Closet</h1>
     <h2 style="font-family:Futura">Organize Your Closet</h2>
+
+    <script>
+    $(document).ready(function(){
+        $("button").click(function(){
+            $(this).toggleClass("clicked");
+        });
+    });
+    </script>
     
     <!-- CSS STUFF -->
     <style type="text/css">
@@ -26,30 +37,30 @@
         width:400px; 
         height: 160px;
         }
-        
-    .buttonClicked {  /* class for clicked buttons*/
-        background:coral;
-       } 
+
+    .clicked{
+        background-color: coral;
+    }
+
     </style>
     <!-- END OF CSS STUFF -->
-
 
     <div id="wrap">
         <div class="left_col">
             <p style="font-family:Futura">Types of Clothing</p>
             <p>Select the Types of clothes in your closet</p>
-            <input type="button" name="type" value="Jeans" onclick="changeColor(this)" />
-            <input type="button" name="type" value="Pants" onclick="changeColor(this)"/>
-            <input type="button" name="type" value="Shorts" onclick="changeColor(this)"/>
-            <input type="button" name="type" value="Skirt" onclick="changeColor(this)"/>
-            <input type="button" name="type" value="Dress" onclick="changeColor(this)"/>
-            <input type="button" name="type" value="Tshirt" onclick="changeColor(this)"/>
-            <input type="button" name="type" value="Shirt" onclick="changeColor(this)"/>
-            <input type="button" name="type" value="Blouse" onclick="changeColor(this)"/>
-            <input type="button" name="type" value="Coat" onclick="changeColor(this)"/>
-            <input type="button" name="type" value="Jacket" onclick="changeColor(this)"/>
-            <input type="button" name="type" value="Athletic Top" onclick="changeColor(this)"/>
-            <input type="button" name="type" value="Athletic Bottom" onclick="changeColor(this)"/>
+            <button id="clicked" name="type">Jeans</button>
+            <button id="clicked" name="type">Pants</button>
+            <button id="clicked" name="type">Shorts</button>
+            <button id="clicked" name="type">Skirt</button>
+            <button id="clicked" name="type">Dress</button>
+            <button id="clicked" name="type">Tshirt</button>
+            <button id="clicked" name="type">Shirt</button>
+            <button id="clicked" name="type">Blouse</button>
+            <button id="clicked" name="type">Coat</button>
+            <button id="clicked" name="type">Jacket</button>
+            <button id="clicked" name="type">Athletic Top</button>
+            <button id="clicked" name="type">Athletic Bottom</button>
         </div>
     </div>
 
@@ -57,18 +68,18 @@
         <div class="right_col">
             <p style="font-family:Futura">Price Range</p>
             <p>Select Price Ranges</p>
-            <input type="button" name="price" value="$10 or less"onclick="changeColor(this)"/>
-            <input type="button" name="price" value="$11-$15" onclick="changeColor(this)"/>
-            <input type="button" name="price" value="$16-$20" onclick="changeColor(this)"/>
-            <input type="button" name="price" value="$21-$30" onclick="changeColor(this)"/>
-            <input type="button" name="price" value="$31-$40" onclick="changeColor(this)"/>
-            <input type="button" name="price" value="$41-$50" onclick="changeColor(this)"/>
-            <input type="button" name="price" value="$51-$60" onclick="changeColor(this)"/>
-            <input type="button" name="price" value="$61-$70" onclick="changeColor(this)"/>
-            <input type="button" name="price" value="$71-$80" onclick="changeColor(this)"/>
-            <input type="button" name="price" value="$81-$100" onclick="changeColor(this)"/>
-            <input type="button" name="price" value="$100-$150" onclick="changeColor(this)"/>
-            <input type="button" name="price" value="$150-$200" onclick="changeColor(this)"/>
+            <button id="clicked" name="price">$10 or less</button>
+            <button id="clicked" name="price">$11-$15</button>
+            <button id="clicked" name="price">$16-$20</button>
+            <button id="clicked" name="price">$21-$30</button>
+            <button id="clicked" name="price">$31-$40</button>
+            <button id="clicked" name="price">$41-$50</button>
+            <button id="clicked" name="price">$51-$60</button>
+            <button id="clicked" name="price">$61-$70</button>
+            <button id="clicked" name="price">$71-$80</button>
+            <button id="clicked" name="price">$81-$100</button>
+            <button id="clicked" name="price">$100-$150</button>
+            <button id="clicked" name="price">$150-$200</button>
         </div
     </div>
 
@@ -76,20 +87,20 @@
         <div class="left_col">
             <p style="font-family:Futura">Color</p>
             <p>Select the colors in your closet</p>
-            <input type="button" name="color" value="White" onclick="changeColor(this)"/>
-            <input type="button" name="color" value="Black" onclick="changeColor(this)"/>
-            <input type="button" name="color" value="Gray" onclick="changeColor(this)"/>
-            <input type="button" name="color" value="Red" onclick="changeColor(this)"/>
-            <input type="button" name="color" value="Orange" onclick="changeColor(this)"/>
-            <input type="button" name="color" value="Yellow" onclick="changeColor(this)"/>
-            <input type="button" name="color" value="Green" onclick="changeColor(this)"/>
-            <input type="button" name="color" value="Blue" onclick="changeColor(this)"/>
-            <input type="button" name="color" value="Purple" onclick="changeColor(this)"/>
-            <input type="button" name="color" value="Brown" onclick="changeColor(this)"/>
-            <input type="button" name="color" value="Peach" onclick="changeColor(this)"/>
-            <input type="button" name="color" value="Mint" onclick="changeColor(this)"/>
-            <input type="button" name="color" value="Magenta" onclick="changeColor(this)"/>
-            <input type="button" name="color" value="Off-White" onclick="changeColor(this)"/>
+            <button id="clicked" name="color">White</button>
+            <button id="clicked" name="color">Black</button>
+            <button id="clicked" name="color">Gray</button>
+            <button id="clicked" name="color">Red</button>
+            <button id="clicked" name="color">Orange</button>
+            <button id="clicked" name="color">Yellow</button>
+            <button id="clicked" name="color">Green</button>
+            <button id="clicked" name="color">Blue</button>
+            <button id="clicked" name="color">Purple</button>
+            <button id="clicked" name="color">Brown</button>
+            <button id="clicked" name="color">Peach</button>
+            <button id="clicked" name="color">Mint</button>
+            <button id="clicked" name="color">Magenta</button>
+            <button id="clicked" name="color">Off-White</button>
         </div>
     </div>
     
@@ -97,19 +108,19 @@
         <div class="right_col">
             <p style="font-family:Futura">Patterns</p>
             <p>Select the patterns in your closet</p>
-            <input type="button" name="pattern" value="Striped" onclick="changeColor(this)"/>
-            <input type="button" name="pattern" value="Multi" onclick="changeColor(this)"/>
-            <input type="button" name="pattern" value="Dots" onclick="changeColor(this)"/>
-            <input type="button" name="pattern" value="Paisley" onclick="changeColor(this)"/>
-            <input type="button" name="pattern" value="Argyle" onclick="changeColor(this)"/>
-            <input type="button" name="pattern" value="Checkered" onclick="changeColor(this)"/>
-            <input type="button" name="pattern" value="Floral" onclick="changeColor(this)"/>
-            <input type="button" name="pattern" value="Brocade" onclick="changeColor(this)"/>
-            <input type="button" name="pattern" value="Houndstooth" onclick="changeColor(this)"/>
-            <input type="button" name="pattern" value="Tartan" onclick="changeColor(this)"/>
-            <input type="button" name="pattern" value="Chevron/ZigZag" onclick="changeColor(this)"/>
-            <input type="button" name="pattern" value="Toile" onclick="changeColor(this)"/>
-            <input type="button" name="pattern" value="Tie-Dye" onclick="changeColor(this)"/>
+            <button id="clicked" name="pattern">Striped</button>
+            <button id="clicked" name="pattern">Multi</button>
+            <button id="clicked" name="pattern">Dots</button>
+            <button id="clicked" name="pattern">Paisley</button>
+            <button id="clicked" name="pattern">Argyle</button>
+            <button id="clicked" name="pattern">Checkered</button>
+            <button id="clicked" name="pattern">Floral</button>
+            <button id="clicked" name="pattern">Brocade</button>
+            <button id="clicked" name="pattern">Houndstooth</button>
+            <button id="clicked" name="pattern">Tartan</button>
+            <button id="clicked" name="pattern">Chevron/ZigZag</button>
+            <button id="clicked" name="pattern">Toile</button>
+            <button id="clicked" name="pattern">Tie-Dye</button>
         </div>
     </div>
     
@@ -117,27 +128,27 @@
         <div class="left_col">
             <p style="font-family:Futura">Clothes Size</p>
             <p>Select the sizes in your closet</p>
-            <input type="button" name="size" value="XXS" onclick="changeColor(this)"/>
-            <input type="button" name="size" value="XS" onclick="changeColor(this)"/>
-            <input type="button" name="size" value="S" onclick="changeColor(this)"/>
-            <input type="button" name="size" value="M" onclick="changeColor(this)"/>
-            <input type="button" name="size" value="L" onclick="changeColor(this)"/>
-            <input type="button" name="size" value="XL" onclick="changeColor(this)"/>
-            <input type="button" name="size" value="XXL" onclick="changeColor(this)"/>
-            <input type="button" name="size" value="00" onclick="changeColor(this)"/>
-            <input type="button" name="size" value="0" onclick="changeColor(this)"/>
-            <input type="button" name="size" value="2" onclick="changeColor(this)"/>
-            <input type="button" name="size" value="4" onclick="changeColor(this)"/>
-            <input type="button" name="size" value="6" onclick="changeColor(this)"/>
-            <input type="button" name="size" value="8" onclick="changeColor(this)"/>
-            <input type="button" name="size" value="10" onclick="changeColor(this)"/>
-            <input type="button" name="size" value="12" onclick="changeColor(this)"/>
-            <input type="button" name="size" value="14" onclick="changeColor(this)"/>
-            <input type="button" name="size" value="16" onclick="changeColor(this)"/>
-            <input type="button" name="size" value="18" onclick="changeColor(this)"/>
-            <input type="button" name="size" value="20" onclick="changeColor(this)"/>
-            <input type="button" name="size" value="22" onclick="changeColor(this)"/>
-            <input type="button" name="size" value="24" onclick="changeColor(this)"/>
+            <button id="clicked" name="size">XXS</button>
+            <button id="clicked" name="size">XS</button>
+            <button id="clicked" name="size">S</button>
+            <button id="clicked" name="size">M</button>
+            <button id="clicked" name="size">L</button>
+            <button id="clicked" name="size">XL</button>
+            <button id="clicked" name="size">XXL</button>
+            <button id="clicked" name="size">00</button>
+            <button id="clicked" name="size">0</button>
+            <button id="clicked" name="size">2</button>
+            <button id="clicked" name="size">4</button>
+            <button id="clicked" name="size">6</button>
+            <button id="clicked" name="size">8</button>
+            <button id="clicked" name="size">10</button>
+            <button id="clicked" name="size">12</button>
+            <button id="clicked" name="size">14</button>
+            <button id="clicked" name="size">16</button>
+            <button id="clicked" name="size">18</button>
+            <button id="clicked" name="size">20</button>
+            <button id="clicked" name="size">22</button>
+            <button id="clicked" name="size">24</button>
         </div>
     </div>
     
@@ -145,14 +156,14 @@
         <div class="right_col">
             <p style="font-family:Futura">Pants Size</p>
             <p>Select the sizes of your jeans and pants in your closet</p>
-            <input type="button" name="pants" value="26" onclick="changeColor(this)"/>
-            <input type="button" name="pants" value="27" onclick="changeColor(this)"/>
-            <input type="button" name="pants" value="28" onclick="changeColor(this)"/>
-            <input type="button" name="pants" value="29" onclick="changeColor(this)"/>
-            <input type="button" name="pants" value="30" onclick="changeColor(this)"/>
-            <input type="button" name="pants" value="31" onclick="changeColor(this)"/>
-            <input type="button" name="pants" value="32" onclick="changeColor(this)"/>
-            <input type="button" name="pants" value="33" onclick="changeColor(this)"/>
+            <button id="clicked" name="pant size">26</button>
+            <button id="clicked" name="pant size">27</button>
+            <button id="clicked" name="pant size">28</button>
+            <button id="clicked" name="pant size">29</button>
+            <button id="clicked" name="pant size">30</button>
+            <button id="clicked" name="pant size">31</button>
+            <button id="clicked" name="pant size">32</button>
+            <button id="clicked" name="pant size">33</button>
         </div>
     </div>
     
@@ -160,14 +171,14 @@
         <div class="left_col">
             <p style="font-family:Futura">Fit</p>
             <p>Select Fit</p>
-            <input type="button" name="fit" value="Regular" onclick="changeColor(this)"/>
-            <input type="button" name="fit" value="Bootcut" onclick="changeColor(this)"/>
-            <input type="button" name="fit" value="Skinny" onclick="changeColor(this)"/>
-            <input type="button" name="fit" value="Low-rise" onclick="changeColor(this)"/>
-            <input type="button" name="fit" value="Flare" onclick="changeColor(this)"/>
-            <input type="button" name="fit" value="Baggy" onclick="changeColor(this)"/>
-            <input type="button" name="fit" value="Fitted" onclick="changeColor(this)"/>
-            <input type="button" name="fit" value="Shift" onclick="changeColor(this)"/>
+            <button id="clicked" name="fit">Regular</button>
+            <button id="clicked" name="fit">Bootcut</button>
+            <button id="clicked" name="fit">Skinny</button>
+            <button id="clicked" name="fit">Low-rise</button>
+            <button id="clicked" name="fit">Flare</button>
+            <button id="clicked" name="fit">Baggy</button>
+            <button id="clicked" name="fit">Fitted</button>
+            <button id="clicked" name="fit">Shift</button>
         </div>
     </div>
     
@@ -177,10 +188,10 @@
         </div>
     </div>
     
-    <script> function changeColor(button) //Javascript function to change background of button once it's been clicked
+    <!--<script> function changeColor(button) //Javascript function to change background of button once it's been clicked
     {
         button.className+= " buttonClicked";
     }
-    </script>
+    </script>-->
 
 </body>
