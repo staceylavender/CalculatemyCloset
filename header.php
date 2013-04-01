26c9cfbb5586775393aea6b86fda16bc0c281a4e
Kendra Yee <cunningham.kendra@gmail.com>
2013-04-01 11:26:59 -0400

    created header file

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+"http://www.w3.org/TR/html4/strict.dtd">
+<head>
+	<title>Calculate My Closet</title>
+	<link type="text/css" rel="stylesheet" href="stylesheet.css">
+	<script type="text/javascript" src="jquery-1.2.6.min.js"></script>
+</head>
+
+<body>
+	<div id="menu">
+		<h1><a href="home.php" accesskey="1"><img src="logo2.png"></a></h1>
