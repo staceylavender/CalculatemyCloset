6a6e3bf33418fef8a524b5f21b63b8707f1681f1
Kendra Yee <cunningham.kendra@gmail.com>
2013-04-01 12:00:26 -0400

    add jquery functionality but not working

--- home.php
+++ home.php
@@ -2,10 +2,26 @@
 <?php include('header.php');?> 
 <head>
     <title>Calculate My Closet</title>
+    <link rel='stylesheet' type='text/css' href='stylesheet.css'/>
 <head>
 <body>
-	<p>
-    <a href="createaccount.php" style="font-family:Futura">Lets Get Started!</a>
-    <a href="about.php" style="font-family:Futura">What is this?</a>
-	</p>
+	<div class="button">
+    	<a href="createaccount.php">Lets Get Started!</a>
+    </div>
+    <div class="button">
+    	<a href="about.php">What is this?</a>
+	</div>
 </body>
+
+<script>
+	$(document).ready(function(){
+    	$('.button').mouseenter(function(){
+        	$('.button').fadeTo('slow', 0.5);
+    	});
+    	$('.button').mouseleave(function(){
+        	$('.button').fadeTo('slow', 1);
+    	});
+	});
+    </script>
+
+</html>
