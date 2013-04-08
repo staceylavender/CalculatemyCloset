9866a75f8caf6f05eafe568c60349425b0c9d10a
Kendra Yee <cunningham.kendra@gmail.com>
2013-04-08 15:59:06 -0400

    added code that saves tablename into session

--- CreateAccount.php
+++ CreateAccount.php
@@ -1,17 +1,19 @@
 <?php
 require_once "login.php";
 session_start();
+
 if ( isset($_POST['firstname']) && isset($_POST['lastname']) 
      && isset($_POST['email'])&& isset($_POST['password'])) 
   {
+
    $firstname = mysql_real_escape_string($_POST['firstname']);
    $lastname = mysql_real_escape_string($_POST['lastname']);
    $email = mysql_real_escape_string($_POST['email']);
    $password = mysql_real_escape_string($_POST['password']);
    $tablename = $firstname.$lastname;
+   $_SESSION ['tablename']=$tablename; //add tablename into session
    $sql = "INSERT INTO users (lastname, firstname, email, password) 
               VALUES ('$lastname', '$firstname', '$email', '$password')";
-	// echo "<pre>\n$sql\n</pre>\n";
 	mysql_query($sql);
 	$sql2 = "CREATE TABLE $tablename(id INT UNSIGNED NOT NULL AUTO_INCREMENT KEY, name VARCHAR(100), type VARCHAR(30), color VARCHAR(30), pattern VARCHAR(30), size VARCHAR(10), fit VARCHAR(30), price VARCHAR(30), worn CHAR(6))";
 	mysql_query($sql2);
