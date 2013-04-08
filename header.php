e71678c5b3813bc520ae2a3e406af65e2fd7a819
Kendra Yee <cunningham.kendra@gmail.com>
2013-04-08 14:12:41 -0400

    Updated header

--- header.php
+++ header.php
@@ -2,10 +2,10 @@
 "http://www.w3.org/TR/html4/strict.dtd">
 <head>
 	<title>Calculate My Closet</title>
-	<link type="text/css" rel="stylesheet" href="stylesheet.css">
-	<script type="text/javascript" src="jquery-1.2.6.min.js"></script>
+	<link rel='stylesheet' type='text/css' href='stylesheet.css'/>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
 </head>
 
 <body>
 	<div id="menu">
-		<h1><a href="home.php" accesskey="1"><img src="logo2.png"></a></h1>
+		<h1><a href="home.php" accesskey="1"><img style="display:block" src="logo.png"></a></h1>
